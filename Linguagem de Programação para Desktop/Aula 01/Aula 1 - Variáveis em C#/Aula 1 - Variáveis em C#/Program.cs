dfebeb425aef4b402f7187f93ed7a14375d28221
Pedro Gustavo Brandão Sá Da Silva <140172385+Goleiro19@users.noreply.github.com>
2024-08-05 11:48:51 -0300

    Add files via upload

---
+++ Linguagem de Programação para Desktop/Aula 01/Aula 1 - Variáveis em C#/Aula 1 - Variáveis em C#/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "Jarvis Baiano APP C#";
+Console.WriteLine("Hello, info07M");
+
+String Nome;
+String Sobrenome;
+Int32 AnoNascimento;
+Int32 AnoAtual;
+
+Console.Write("Digite o seu nome: ");
+Nome = Console.ReadLine();
+
+Console.Write("Digite o seu sobrenome: ");
+Sobrenome = Console.ReadLine();
+
+Console.WriteLine("Olá como está ? " + Nome + " " + Sobrenome);
+
+Console.Write("Digite o ano de nascimento: ");
+AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o ano atual: ");
+AnoAtual = Convert.ToInt32(Console.ReadLine());
+
+Int32 IdadeCalculada = AnoAtual - AnoNascimento;
+
+Console.WriteLine("Nossa esta com essa idade! " + IdadeCalculada + "Anos");
+Console.Beep();
+
+Console.ReadKey();
+
+Decimal ValorHora = 0;
+Decimal HorasTrabalhadas = 0;
+Decimal ValorReceber = 0;
+
+Console.Write("Informe seu valor/hora: ");
+ValorHora = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Informe a quantidade de horas trabalhadas: ");
+HorasTrabalhadas = Convert.ToDecimal(Console.ReadLine());
+
+ValorReceber = HorasTrabalhadas * ValorHora;
+
+Console.WriteLine(Nome + ", Valor a Receber: R$ " + ValorReceber);
+Console.Beep();
+
+Console.ReadKey();
